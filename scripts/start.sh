bc4ba175e14886d4f602392380871a99989178f8
KavinduGeesara <kavindugp@gmail.com>
2025-06-07 08:30:10 +0000

    Modify Docker start shell script to ignore network creation is network exists

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,9 @@ DOCKER_USERNAME="kgp9588"
 docker stop ocr-model ocr-gateway 2>/dev/null
 docker rm ocr-model ocr-gateway 2>/dev/null
 
-docker network create ocr-network 2>/dev/null || true
+if ! docker network ls --format '{{.Name}}' | grep -wq ocr-network; then
+  docker network create ocr-network
+fi
 
 docker run -d --name ocr-model -p 8080:8080 --network ocr-network $DOCKER_USERNAME/ocr-model:latest
 docker run -d --name ocr-gateway -p 8001:8001 --network ocr-network $DOCKER_USERNAME/ocr-gateway:latest
